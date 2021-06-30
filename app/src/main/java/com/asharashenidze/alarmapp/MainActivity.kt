983765681a6be6e76255b0cae881124256d794e8
acho01 <archil.sharashenidze@azry.com>
2021-06-30 20:03:45 +0400

    MVP pattern skeleton added

--- app/src/main/java/com/asharashenidze/alarmapp/MainActivity.kt
+++ app/src/main/java/com/asharashenidze/alarmapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.asharashenidze.alarmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asharashenidze.alarmapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/asharashenidze/alarmapp/presenter/IMainPresenter.kt
@@ -0,0 +1,4 @@
+package com.asharashenidze.alarmapp.presenter
+
+interface IMainPresenter {
+}
